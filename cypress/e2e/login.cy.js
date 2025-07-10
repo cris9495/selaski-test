a4dfdcc367a95b102776f97899cb8a6e0ccdbf24
Crisnayler <crisnita204@utp.edu.co>
2025-07-09 22:01:35 -0500

    add only

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -6,4 +6,11 @@ describe('Login ', () => {
     LoginPage.login('5', '5','6','9' );
     LoginPage.checkLoginSuccess();
   });
+
+  it.only('Should show message error', () => {
+    LoginPage.visit();
+    LoginPage.login('5', '4','8','9' );
+    LoginPage.checkLoginSuccess();
+  });
+  
 });
