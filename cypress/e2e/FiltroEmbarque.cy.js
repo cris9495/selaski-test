91da9f79ab80eca9f80cad7c42c20f08b366aa28
Crisnayler <crisnita204@utp.edu.co>
2025-07-12 14:43:01 -0500

    Refactor FiltroEmbarque tests and enhance FiltroPage functionality

--- cypress/e2e/FiltroEmbarque.cy.js
+++ cypress/e2e/FiltroEmbarque.cy.js
@@ -21,16 +21,35 @@ describe('FiltroEmbarque', ()=>{
         FiltroPage.FieldEscribaStatus('be.visible');
       });
 
-    it('Should type and find coincidence', () => {    
+    it('Should close the filter aplied', () => {    
         
         FiltroPage.OptionEmbarque();
         FiltroPage.FieldEscribaStatus('be.visible');
         FiltroPage.FieldEscriba('CALUMET-01');
-        FiltroPage.ShowAplyFilter('CALUMET-01');
-        FiltroPage.ShowCoincidence('CALUMET-01');
-        FiltroPage.closefilter('CALUMET-01');
+        FiltroPage.ShowAplyFilter('CALUMET-01');        
+        FiltroPage.closefilter('CALUMET-01');          
+       
+      });
+
+    it('Should catch coincidence', () => {    
+        
+        FiltroPage.OptionEmbarque();
+        FiltroPage.FieldEscribaStatus('be.visible');
+        FiltroPage.FieldEscriba('KSIO34596');
+        FiltroPage.ShowAplyFilter('KSIO34596');        
+        FiltroPage.checkEmbarqueValues(['KSIO34596-02','KSIO34596-01']); 
+                     
+       
+      });
+
+      it('Should not catch coincidence', () => {    
+        
+        FiltroPage.OptionEmbarque();
+        FiltroPage.FieldEscribaStatus('be.visible');
         FiltroPage.FieldEscriba('CALUMET-58');
-        FiltroPage.ShowNotCoincidence();        
+        FiltroPage.ShowAplyFilter('CALUMET-58');
+        FiltroPage.ShowNotCoincidence();  
+        FiltroPage.closefilter('CALUMET-58');       
        
       });
 });
--- cypress/support/pages/FiltroPage.js
+++ cypress/support/pages/FiltroPage.js
@@ -1,5 +1,3 @@
-
-
 class FiltroPage {
   Filtros() {
     cy.get('#orders-container > app-organism-filters-tags > span > app-atom-filter-tab > div > p',{ timeout: 10000 }).should('contain', 'Filtros').click(); 
@@ -10,6 +8,7 @@ class FiltroPage {
   }
 
   OptionEmbarque(){
+    cy.get('[class="w-full shadow z-10"]',{ timeout: 10000 }).should('exist');
     cy.get('button[class="onclick_emitter border-none w-36"]',{ timeout: 10000 }).click() //option Seleccionar  
     cy.wait(2000);  
     cy.get('div.search-options', { timeout: 20000 }).should('be.visible');  // wait for dropdown container      
@@ -27,24 +26,48 @@ class FiltroPage {
     
   }
 
-  closefilter(information){    
+  closefilter(information){ 
+    cy.wait(1000);   
     cy.contains('p.truncate', `Embarque: ${information}`)
     .parents('div.max-w-xs')                          // Go up to the tag container
     .find('span.tag-icon')                            //Find the remove icon inside it
     .click();  
 
-    cy.contains('p.truncate', 'Embarque: CALUMET-01').should('not.exist');    
+    cy.contains('p.truncate', `Embarque: ${information}`).should('not.exist');    
   }
-
-  ShowCoincidence(information){
-    cy.get("p[title='CALUMET-01']").should('contain',information)
-  }
+  
 
   ShowNotCoincidence(){
     cy.get('.text-center.text-lg.font-medium.mt-6').should('contain','Sin datos para mostrar')
   }
 
-  
+  catchrowsembarque() {
+    const embarqueValues = [];
+     return cy.get('#table-results-container').should('have.length.greaterThan', 0)
+      .get('#table-results-container')
+      .find('app-molecule-table-first-element-list app-molecule-table-first-element')
+      .each(($cell) => {
+        cy.wrap($cell)
+          .find('p.truncate')
+          .invoke('text')
+          .then((text) => {
+            embarqueValues.push(text.trim());
+          });
+      })
+      .then(() => {
+        cy.log('Valores de Embarque:', JSON.stringify(embarqueValues));
+        return cy.wrap(embarqueValues); // Return as Cypress chainable
+      });
+  }
+
+  checkEmbarqueValues(expectedValues) {
+    this.catchrowsembarque().then((actualValues) => {
+      expect(actualValues).to.deep.equal(expectedValues); //how add assertion to check if the values match cypress
+      console.log('Valores de Embarque verificados:', JSON.stringify(actualValues));
+      cy.log('Valores de Embarque verificados:', JSON.stringify(actualValues)); //when execute cypress i don't see the log, help @workaround
+
+    });
+  }
 
 }
 
