8f295a8f2138240ffae0364716d914eef5d89e3c
Crisnayler <crisnita204@utp.edu.co>
2025-07-12 15:37:41 -0500

    Fix uppercase letter

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -13,12 +13,12 @@ describe('Login ', () => {
     LoginPage.showErrorInvalid();
   });
 
-  it('type alphanumeric, Should button be disable', () => {    
+  it('Type alphanumeric, Should button be disable', () => {    
     LoginPage.login('5', 'f','6','e' );
     LoginPage.checkbutton('be.disabled');
   });
 
-  it('not type values, Should button be disable', () => {    
+  it('Not type values, Should button be disable', () => {    
     LoginPage.checkbutton('be.disabled');
     
   });
